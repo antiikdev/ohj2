f2f97052313aa5e00e9967ebd0b218db88585981
Kotilainen Ilkka Antti <ilkka.a.kotilainen@gmail.com>
2021-09-28 20:31:11 +0300

    vaihe2 ver1

---
+++ src/vaihe2/KyselyGUIController.java
@@ -0,0 +1,10 @@
+package vaihe2;
+
+/**
+ * @author ilkka
+ * @version 28.9.2021
+ *
+ */
+public class KyselyGUIController {
+      // TODO
+}
---
+++ src/vaihe2/KyselyMain.java
@@ -0,0 +1,38 @@
+package vaihe2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author Antiikdev & Doomslizer
+ * @version 28.9.2021
+ *
+ */
+public class KyselyMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("KyselyGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final KyselyGUIController kyselyCtrl = (KyselyGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("kysely.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("kysely");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
