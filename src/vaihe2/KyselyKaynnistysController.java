45cba908c5626edb765675b0408573433f0a9de5
Kotilainen Ilkka Antti <ilkka.a.kotilainen@gmail.com>
2021-10-15 17:23:44 +0300

    tyo3 paivitys

--- src/vaihe2/KyselyKaynnistysController.java
+++ src/vaihe2/KyselyKaynnistysController.java
@@ -2,6 +2,7 @@ package vaihe2;
 
 import fi.jyu.mit.fxgui.Dialogs;
 import fi.jyu.mit.fxgui.ModalController;
+import fi.jyu.mit.fxgui.ModalControllerInterface;
 import javafx.fxml.FXML;
 
 /**
@@ -11,7 +12,7 @@ import javafx.fxml.FXML;
  * @version 14.10.2021
  *
  */
-public class KyselyKaynnistysController
+public class KyselyKaynnistysController implements ModalControllerInterface<String>
 {    
     //ok nappi
     @FXML void ok()
@@ -23,6 +24,24 @@ public class KyselyKaynnistysController
     @FXML void sulje()
     {
         Dialogs.showMessageDialog("Et oo menossa minnekään!");
+    }
+
+    @Override
+    public String getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void handleShown() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDefault(String arg0) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
